fc7b7648bf64fd2b9eed22e77a7b643064e677dc
saliou33 <losaliou.sl33@gmail.com>
2023-03-01 10:25:52 +0000

    Update Routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import './App.css';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import HomePatient from './pages/patient/Home';
+import HomeMedecin from './pages/medecin/Home';
+import HomeAdmin from './pages/admin/Home';
 import RvPatient from './pages/patient/sections/Rv';
 import DmPatient from './pages/patient/sections/Dm';
+
 import Constants from './utils/constants';
 
 const App = () => {
@@ -20,9 +23,13 @@ const App = () => {
       {mapToRoute(Constants.LOGIN_ROUTES, Login)}
       {mapToRoute(Constants.SIGNUP_ROUTES, Signup)}
       <Route path="/p/" element={<HomePatient />}>
+        <Route index element={<DmPatient />} />
         <Route path="dm" element={<DmPatient />} />
         <Route path="rv" element={<RvPatient />} />
       </Route>
+
+      <Route path="/m/" element={<HomeMedecin />} />
+      <Route path="/a/" element={<HomeAdmin />} />
     </Routes>
   );
 };
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Logo from '../../components/Logo';
+import Constants from '../../utils/constants';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen flex flex-col">
+      <div className="mb-2 px-3 py-4 shadow-sm rounded-lg m-1 justify-between bg-primary5 ">
+        <Logo />
+      </div>
+
+      <div className="flex flex-1 gap-3 py-3 px-1">
+        <div className="flex relative flex-col basis-2/12 sm:3/12 max-w-[14rem] shadom-sm rounded-lg px-3 py-4 bg-yellow3 border-b-2 border-t-2 shadow-lg border-primary5 opacity-90 gap-4 brightness-125">
+          {Constants.ADMIN_LINKS.map((e, k) => (
+            <>
+              <a
+                key={k}
+                href={e.path}
+                className="hover:text-primary5 cursor-pointer text-black"
+              >
+                {e.name}
+              </a>
+            </>
+          ))}
+
+          <button className="absolute bottom-5">Deconnexion</button>
+        </div>
+
+        <div className="flex-1 bg-white shadow-sm rounded-lg px-3 py-4">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
